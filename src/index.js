6819b2efbdc4f817c439635a96470cef15f5cec0
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-09 11:42:44 +0200

    imported files and put everything together

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {
+    choice,
+    remove
+} from './helpers';
+import fruits from './foods';
+
+const fruit = choice(fruits);
+
+console.log(`I’d like one ${fruit}, please.`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+const leftFruits = remove(fruits, fruit);
+
+console.log(`I’m sorry, we’re all out. We have ${leftFruits.length} left.`)
